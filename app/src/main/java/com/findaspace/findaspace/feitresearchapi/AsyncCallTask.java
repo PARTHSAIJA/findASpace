49e2a4b6d72d87984cb412f05d94fc7ae42419c3
Luke Phillips <12614172@student.uts.edu.au>
2018-10-06 23:10:05 +1000

    AsyncCallTask.java added async comments

--- app/src/main/java/com/findaspace/findaspace/feitresearchapi/AsyncCallTask.java
+++ app/src/main/java/com/findaspace/findaspace/feitresearchapi/AsyncCallTask.java
@@ -23,38 +23,42 @@ public class AsyncCallTask extends AsyncTask<Void, Void, String> {
     }
 
     /**
-     * @brief
+     * @brief Async task to get the JSON from the FEIT People Count Sensor
      * @param voids
      * @return
      */
     @Override
     protected String doInBackground(Void... voids) {
         try{
-            //TODO: Currently API URL hardcoded
+            //TODO: Currently API URL hardcoded. Need to pass in the API ParametersRecord to complete the URL string
             String sensorAPIURL = "http://eif-research.feit.uts.edu.au/api/json/?rFromDate=2018-09-16T18%3A05%3A00&rToDate=2018-09-18T18%3A05%3A00&rFamily=people&rSensor=+PC00.08+%28In%29";
 
+            //Set up URL connection
             URL url = new URL(sensorAPIURL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
+            connection.connect();       //Commence connection
 
+            //Get the outputted URL contents and put into the buffer
             InputStream inputstream = connection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputstream));
 
+            //Transfer buffer into a string builder
             StringBuilder strBuilder = new StringBuilder();
-
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 strBuilder.append(line);
             }
 
+            //Convert the string building into a JSON Array
             JsonElement je = new JsonParser().parse(strBuilder.toString());
             JsonArray myArray = je.getAsJsonArray();
 
-            //["2018-09-18 18:00:00",88]
+            //Get the last position in the JSON Array (most current people count)  eg - ["2018-09-18 18:00:00",88]
             System.out.println("OUTPUT: " + myArray.get(myArray.size()-1).isJsonObject());
             System.out.println("OUTPUT_JSON_Object: " + myArray.get(myArray.size()-1));
             String currentPeopleCountStr = myArray.get(myArray.size()-1).toString();
 
+            //Extract the DateTime as a String - 2018-09-18 18:00:00
             String [] dateStrToken = currentPeopleCountStr.split ("\"");
             String dateStr = dateStrToken[1];
             System.out.println("OUTPUT_Date: " + dateStr);
@@ -67,6 +71,7 @@ public class AsyncCallTask extends AsyncTask<Void, Void, String> {
 //            //Date date = new Date(df.);
 //            System.out.println("OUTPUT_Time: " + DateFormat.getTimeInstance(DateFormat.MEDIUM).format(dateStr));
 
+            //Extract the PeopleCount as a String 88
             String peopleCountStr = (currentPeopleCountStr.substring(0, currentPeopleCountStr.length() - 1));
             peopleCountStr = peopleCountStr.substring(peopleCountStr.lastIndexOf(",") + 1);
             System.out.println("OUTPUT_PeopleCount: " + peopleCountStr);
